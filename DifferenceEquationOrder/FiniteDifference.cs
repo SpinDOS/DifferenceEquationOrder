a81002dc6c78ab94bbd648026c43c68fbf2e83aa
Alexander <Alexander@ALEXANDER-PC>
2017-01-18 04:07:26 +0300

    Add minimumH and maximumH properties

--- DifferenceEquationOrder/FiniteDifference.cs
+++ DifferenceEquationOrder/FiniteDifference.cs
@@ -9,14 +9,22 @@ namespace DifferenceEquationOrder
     public sealed class FiniteDifference
     {
         private FiniteDifference() { }
+
+        public int MinimumH { get; private set; }
+        public int MaximumH { get; private set; }
         public int Order { get; private set; }
 
         public static FiniteDifference GetFiniteDifferenceByOrder(int order)
+            => GetFiniteDifferenceByOrderAndMinH(order, 0);
+
+        public static FiniteDifference GetFiniteDifferenceByOrderAndMinH(int order, int minH)
         {
             if (order < 0)
                 throw new ArgumentException("Finite difference order can not be less than zero");
             FiniteDifference result = new FiniteDifference();
             result.Order = order;
+            result.MinimumH = minH;
+            result.MaximumH = minH + order;
             return result;
         }
     }
--- UnitTestProject/FiniteDifferenceTests.cs
+++ UnitTestProject/FiniteDifferenceTests.cs
@@ -15,6 +15,23 @@ namespace UnitTestProject
         {
             FiniteDifference dif = FiniteDifference.GetFiniteDifferenceByOrder(order);
             Assert.Equal(order, dif.Order);
+            Assert.Equal(0, dif.MinimumH);
+            Assert.Equal(order, dif.MaximumH);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(-2)]
+        [InlineData(5)]
+        public void SetMinH(int minH)
+        {
+            Random rnd = new Random();
+            int order = rnd.Next(10);
+            FiniteDifference dif = FiniteDifference.GetFiniteDifferenceByOrderAndMinH(order, minH);
+            Assert.Equal(minH, dif.MinimumH);
+            Assert.Equal(order, dif.Order);
+            Assert.Equal(minH + order, dif.MaximumH);
         }
     }
 }
