e016640abd3b19a831d50c40830ef9b65fd31700
Alexander <Alexander@ALEXANDER-PC>
2017-01-18 04:04:41 +0300

    Add finite difference creation Add order property

---
+++ DifferenceEquationOrder/FiniteDifference.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferenceEquationOrder
+{
+    public sealed class FiniteDifference
+    {
+        private FiniteDifference() { }
+        public int Order { get; private set; }
+
+        public static FiniteDifference GetFiniteDifferenceByOrder(int order)
+        {
+            if (order < 0)
+                throw new ArgumentException("Finite difference order can not be less than zero");
+            FiniteDifference result = new FiniteDifference();
+            result.Order = order;
+            return result;
+        }
+    }
+}
--- UnitTestProject/FiniteDifferenceTests.cs
+++ UnitTestProject/FiniteDifferenceTests.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using DifferenceEquationOrder;
+using Xunit;
 
 namespace UnitTestProject
 {
     public class FiniteDifferenceTests
     {
-        public void TestMethod1()
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(5)]
+        public void CheckOrderAfterCreating(int order)
         {
+            FiniteDifference dif = FiniteDifference.GetFiniteDifferenceByOrder(order);
+            Assert.Equal(order, dif.Order);
         }
     }
 }
